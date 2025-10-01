237ab8b05ca30316600ebc60c3b2e413e80c0474
Bridget Hartin Pasco <isaacpasco@icloud.com>
2025-10-01 13:47:33 -0700

    the worst font of all time

--- towngen/src/draw.rs
+++ towngen/src/draw.rs
@@ -1,5 +1,5 @@
 use std::error::Error;
-
+pub use crate::letters::*;
 use minifb::{Key, Window, WindowOptions};
 pub use crate::math::*;
 #[derive(Copy, Clone)]
@@ -10,6 +10,8 @@ pub struct Color {
     pub r: u8,
     pub a: u8,
 }
+pub const BLACK:Color = Color{r:0, g:0, b:0, a:255};
+pub const WHITE:Color = Color{r:220, g:220, b:220, a:255};
 #[derive(Clone)]
 pub struct Image {
     pub height: usize,
@@ -17,7 +19,7 @@ pub struct Image {
     pub data: Box<[Color]>,
 }
 impl Image {
-    pub fn new(height: usize, width: usize) -> Self {
+    pub fn new(width: usize, height:usize) -> Self {
         let mut vs = Vec::new();
         vs.reserve_exact(height * width);
         for _ in 0..height * width {
@@ -63,9 +65,7 @@ impl Image {
     }
 
     pub fn clear(&mut self, color: Color) {
-        for i in &mut self.data {
-            *i = color;
-        }
+        self.data.fill(color);
     }
     pub fn export(&self, to: &str) {
         let mut out = String::from("P3\n");
@@ -129,8 +129,6 @@ impl Image {
     }  
     pub fn draw_triangle_shader<Shade:Shader>(&mut self, p1: Vec2, p2: Vec2, p3: Vec2, uv1:Vec2r,uv2:Vec2r, uv3:Vec2r,shader:&Shade) {
         let bx = BB::from_points(&[p1, p2, p3]);
-        //for y in bx.y..bx.y + bx.h + 1 {
-        //   for x in bx.x..bx.x + bx.w + 1 {
         for y in bx.y..bx.y+bx.h {
             for x in bx.x..bx.x+bx.w {
                 let v = Vec2 { x, y };
@@ -148,6 +146,17 @@ impl Image {
             }
         }
     }
+    pub fn draw_bitmap(&mut self, x:i32, y:i32, bmp:&Bitmap, color:Color){
+        let x0 = x as usize;
+        let y0 = y as usize;
+        for yp in 0..bmp.height{
+            for xp in 0..bmp.width{
+                if bmp.get(xp, yp){
+                    self.draw_pixel(xp+x0, yp+y0, color);
+                }
+            }
+        }
+    }
     pub fn draw_rect_shader<Shade:Shader>(&mut self, x:i32, y:i32, w:i32, h:i32, shader:&Shade){
         for yp in y..y+h{
             for xp in x..x+w{
@@ -160,7 +169,6 @@ impl Image {
         let dx = Vec2r::new(w as f32/2.0, h as f32/2.0);
         let dy = Vec2r::new(w as f32/2.0, -h  as f32/2.0);
         let rotr = Mat2::rotmat(rot);
-        println!("{:#?}",rotr);
         let xp = (rotr*dx).to_int();
         let yp = (rotr*dy).to_int();
         let c = Vec2::new(x,y);
@@ -168,7 +176,6 @@ impl Image {
         let p2 = c-yp;
         let p3 = c+yp;
         let p4 = c-xp;
-        println!("{:#?},{:#?}. {:#?}, {:#?}",p1, p2, p3, p4);
         let u1 = Vec2r::new(0.0, 0.0);
         let u2 = Vec2r::new(1.0, 0.0);
         let u3 = Vec2r::new(0.0, 1.0);
@@ -192,8 +199,50 @@ impl Image {
         }
         Ok(out)
     }
+    pub fn draw_char(&mut self,x:i32, y:i32, c:char, color:Color){
+        let bmp = char_to_bmp(c);
+        self.draw_bitmap(x, y, bmp, color);
+    }
+    pub fn draw_text(&mut self, x:i32, y:i32, text:&str, color:Color){
+        let mut cx = x;
+        let mut cy = y;
+        for i in text.chars(){
+            if i == '\n'{
+                cy += TEXT_SPACE;
+                cx = x;
+            }else if i == '\t'{
+                cx += (TEXT_SPACE)*3;
+            }else{
+                self.draw_char(cx, cy, i, color);
+                cx += TEXT_SPACE;
+
+            }
+        }
+    }
 }
 
 pub trait Shader{
     fn kernel(&self,screen_location:Vec2, text_coord:Vec2r)->Color;
+}
+
+#[derive(Clone)]
+pub struct Bitmap{
+    pub width:usize,
+    pub height:usize,
+    pub data:&'static [u8],
+}
+impl Bitmap{
+    pub const fn from_data<const W:usize, const H:usize>(inp:&'static [[u8;W];H])->Self{
+        let data = unsafe{std::slice::from_raw_parts(inp.as_ptr() as * const u8, W*H)};
+        Self { width: W, height: H, data}
+    }
+    pub const fn get(&self,x:usize, y:usize)->bool{
+        if x>=self.width || y>= self.height{
+            return false;
+        }
+        self.data[y*self.width+x] != 0
+    }
+    pub const fn getf(&self, x:f32, y:f32)->bool{
+        self.get((x*self.width as f32) as usize, (y*self.width as f32) as usize)
+    }
 }
--- towngen/src/main.rs
+++ towngen/src/main.rs
@@ -1,8 +1,10 @@
 pub mod draw;
 pub mod math;
-use minifb::{Key, Window, WindowOptions};
+pub mod letters;
+pub use minifb::{Key, Window, WindowOptions};
 
-use crate::draw::{Color, Image, Shader, Vec2, Vec2r};
+use crate::draw::BLACK;
+pub use crate::draw::{Color, Image, Shader, Vec2, Vec2r};
 struct Shade{
     tex:Image
 }
@@ -30,17 +32,22 @@ pub fn spinny(){
         });
         window.set_target_fps(60);
         let mut rt = 0.0;
+        let mut timer = std::time::SystemTime::now();
         let sh = Shade::new("nyancat.png");
         while window.is_open() && !window.is_key_down(Key::Escape) {
-                img.clear(Color {
+               img.clear(Color {
                     r: 255,
                     g: 255,
                     b: 255, 
                     a: 255,
                 });
                 rt += 1.0/60.0;
-                img.draw_rect_rot(500,500, 400,400,rt ,&sh);
+                img.draw_rect_rot(320, 240, 120, 120,rt ,&sh);
+                img.draw_text(100, 320, "howdy :3, THE QUICK BROWN FOX JUMPED OVER THE LAZY DOG\nthe quick brown fox jumped over the lazy dog", BLACK);
             img.draw(&mut window);
+            let next =std::time::SystemTime::now(); 
+            println!("{:#?}", next.duration_since(timer));
+            timer = next;
         }
 }
 pub fn old(){
