101313ed6bfc0e8a2b89c1c1cf99945f340ea79f
Bridget Hartin Pasco <isaacpasco@icloud.com>
2025-09-30 23:07:35 -0700

    real-time

--- towngen/src/draw.rs
+++ towngen/src/draw.rs
@@ -1,139 +1,14 @@
+use std::error::Error;
+
+use minifb::{Key, Window, WindowOptions};
+pub use crate::math::*;
 #[derive(Copy, Clone)]
+#[repr(C)]
 pub struct Color {
-    pub r: u8,
-    pub g: u8,
     pub b: u8,
+    pub g: u8,
+    pub r: u8,
     pub a: u8,
-}
-#[derive(Copy, Clone, PartialEq, Eq)]
-pub struct Vec2 {
-    pub x: i32,
-    pub y: i32,
-}
-impl Vec2 {
-    pub fn new(x: i32, y: i32) -> Self {
-        return Self { x, y };
-    }
-    pub fn dot(&self, other: Self) -> i32 {
-        self.x * other.x + self.y * other.y
-    }
-}
-impl std::ops::Add for Vec2 {
-    type Output = Self;
-    fn add(self, other: Self) -> Self::Output {
-        Self {
-            x: self.x + other.x,
-            y: self.y + other.y,
-        }
-    }
-}
-impl std::ops::Sub for Vec2 {
-    type Output = Self;
-    fn sub(self, other: Self) -> Self::Output {
-        Self {
-            x: self.x - other.x,
-            y: self.y - other.y,
-        }
-    }
-}
-impl std::ops::AddAssign for Vec2 {
-    fn add_assign(&mut self, other: Self) {
-        self.x += other.x;
-        self.y += other.y;
-    }
-}
-impl std::ops::SubAssign for Vec2 {
-    fn sub_assign(&mut self, other: Self) {
-        self.x -= other.x;
-        self.y -= other.y;
-    }
-}
-impl std::ops::Mul<i32> for Vec2 {
-    type Output = Self;
-    fn mul(self, v: i32) -> Self::Output {
-        Self {
-            x: self.x * v,
-            y: self.y * v,
-        }
-    }
-}
-impl std::ops::Div<i32> for Vec2 {
-    type Output = Self;
-    fn div(self, v: i32) -> Self::Output {
-        Self {
-            x: self.x / v,
-            y: self.y / v,
-        }
-    }
-}
-impl std::ops::Rem<i32> for Vec2 {
-    type Output = Self;
-    fn rem(self, v: i32) -> Self::Output {
-        Self {
-            x: self.x % v,
-            y: self.y % v,
-        }
-    }
-}
-impl std::ops::MulAssign<i32> for Vec2 {
-    fn mul_assign(&mut self, v: i32) {
-        self.x *= v;
-        self.y *= v;
-    }
-}
-impl std::ops::DivAssign<i32> for Vec2 {
-    fn div_assign(&mut self, v: i32) {
-        self.x /= v;
-        self.y /= v;
-    }
-}
-impl std::ops::RemAssign<i32> for Vec2 {
-    fn rem_assign(&mut self, v: i32) {
-        self.x %= v;
-        self.y %= v;
-    }
-}
-pub struct BB {
-    pub x: i32,
-    pub y: i32,
-    pub w: i32,
-    pub h: i32,
-}
-impl BB {
-    pub fn intersects(&self, p: Vec2) -> bool {
-        (p.x >= self.x && p.x < (self.x + self.w)) && (p.y >= self.y && p.y < (self.y + self.h))
-    }
-    pub fn from_points(p: &[Vec2]) -> Self {
-        let mut min = p[0];
-        let mut max = p[0];
-        for i in p {
-            if i.x > max.x {
-                max.x = i.x;
-            }
-            if i.y > max.y {
-                max.y = i.y;
-            }
-            if i.x < min.x {
-                min.x = i.x
-            }
-            if i.y < min.y {
-                min.y = i.y;
-            }
-        }
-        Self {
-            x: min.x,
-            y: min.y,
-            w: max.x - min.x,
-            h: max.y - min.y,
-        }
-    }
-}
-pub fn in_triangle(p1: Vec2, p2: Vec2, p3: Vec2, pos: Vec2) -> bool {
-    fn same_side(ptest: Vec2, start: Vec2, end: Vec2, mid: Vec2) -> bool {
-        let m = (end - start) / 2;
-        let mp = mid - m;
-    }
-    todo!()
 }
 #[derive(Clone)]
 pub struct Image {
@@ -159,15 +34,166 @@ impl Image {
             data: vs.into(),
         }
     }
-    pub fn set_pixel(&mut self, x: usize, y: usize, color: Color) {
+    pub fn draw_pixel(&mut self, x: usize, y: usize, color: Color) {
+        if x>=self.width || y>=self.height{
+            return;
+        }
         let c = &mut self.data[y * self.width + x];
         *c = color;
     }
-    pub fn get_pixel(&self, x: usize, y: usize) -> Color {
+    pub fn get_pixel(&self, mut x: usize, mut y: usize) -> Color {
+        if x>=self.width {
+            x = self.width-1;
+        }
+        if y>=self.height{
+            y = self.height-1;
+        }
         self.data[y * self.width + x]
     }
-
-    pub fn draw_triangle(&self, p1: Vec2, p2: Vec2, p3: Vec2) {
-        todo!()
+    pub fn get(&self, x:f64, y:f64)->Color{
+        let mut x0 = (x*self.width as f64) as usize;
+        let mut y0 = (y*self.height as f64) as usize;
+        if x0>=self.width {
+            x0 = self.width-1;
+        }
+        if y0>=self.height{
+            y0 = self.height-1;
+        }
+        self.data[y0*self.width+x0]
     }
+
+    pub fn clear(&mut self, color: Color) {
+        for i in &mut self.data {
+            *i = color;
+        }
+    }
+    pub fn export(&self, to: &str) {
+        let mut out = String::from("P3\n");
+        out += &format!("{} {}\n", self.width, self.height);
+        out += "255\n";
+        for i in &self.data {
+            out += &format!("{} {} {}\n", i.r, i.g, i.b);
+        }
+        std::fs::write(to, out).unwrap();
+    }
+    pub fn draw(&self,window:&mut Window){
+                let buffptr = self.data.as_ptr()as *mut u32;
+                let buffer = unsafe{
+                std::slice::from_raw_parts_mut(buffptr, self.height*self.width)
+                };
+                window
+                .update_with_buffer(&buffer, self.width, self.height)
+                .unwrap();
+    }
+    pub fn draw_forever(&self) {
+        let mut window = Window::new(
+            "Test - ESC to exit",
+        self.width as usize,
+            self.height as usize,
+            WindowOptions::default(),
+        )
+        .unwrap_or_else(|e| {
+            panic!("{}", e);
+        });
+        window.set_target_fps(60);
+        let buffptr = self.data.as_ptr()as *mut u32;
+        let buffer = unsafe{
+            std::slice::from_raw_parts_mut(buffptr, self.height*self.width)
+        };
+        while window.is_open() && !window.is_key_down(Key::Escape) {
+            window
+                .update_with_buffer(&buffer, self.width, self.height)
+                .unwrap();
+        }
+    }
+    pub fn draw_triangle(&mut self, p1: Vec2, p2: Vec2, p3: Vec2, color: Color) {
+        let bx = BB::from_points(&[p1, p2, p3]);
+        //for y in bx.y..bx.y + bx.h + 1 {
+        //   for x in bx.x..bx.x + bx.w + 1 {
+        for y in bx.y..bx.y+bx.h {
+            for x in bx.x..bx.x+bx.w {
+                let v = Vec2 { x, y };
+                if in_triangle(p1, p2, p3, v) {
+                    self.draw_pixel(v.x as usize, v.y as usize, color);
+                }
+            }
+        }
+    }
+    pub fn calc_uvs_lin(p1:Vec2, p2:Vec2, p3:Vec2, uv1:Vec2r, uv2:Vec2r, uv3:Vec2r, pos:Vec2)->Vec2r{ 
+        let denum = triangle_area(p1, p2, p3) as f32;
+        let t1 = triangle_area(pos, p2, p3) as f32/denum;
+        let t2 = triangle_area(pos, p3, p1) as f32/denum;
+        let t3 = triangle_area(pos, p2, p1) as f32/denum;
+      // println!("{t1}, {t2},{t3},{denum}, {}", t1+t2+t3);
+        uv1*t1+uv2*t2+uv3*t3
+    }  
+    pub fn draw_triangle_shader<Shade:Shader>(&mut self, p1: Vec2, p2: Vec2, p3: Vec2, uv1:Vec2r,uv2:Vec2r, uv3:Vec2r,shader:&Shade) {
+        let bx = BB::from_points(&[p1, p2, p3]);
+        //for y in bx.y..bx.y + bx.h + 1 {
+        //   for x in bx.x..bx.x + bx.w + 1 {
+        for y in bx.y..bx.y+bx.h {
+            for x in bx.x..bx.x+bx.w {
+                let v = Vec2 { x, y };
+                if in_triangle(p1, p2, p3, v) {
+                    let color = shader.kernel(v, Self::calc_uvs_lin(p1, p2, p3, uv1, uv2, uv3, v));
+                    self.draw_pixel(v.x as usize, v.y as usize, color);
+                }
+            }
+        }
+    }
+    pub fn draw_rect(&mut self, x:i32, y:i32, w:i32, h:i32,color:Color){
+        for yp in y..y+h{
+            for xp in x..x+w{
+                self.draw_pixel(xp as usize, yp as usize, color);
+            }
+        }
+    }
+    pub fn draw_rect_shader<Shade:Shader>(&mut self, x:i32, y:i32, w:i32, h:i32, shader:&Shade){
+        for yp in y..y+h{
+            for xp in x..x+w{
+                let color = shader.kernel(Vec2::new(xp, yp), Vec2r::new((xp-x) as f32/(w as f32), (yp-y) as f32/(h as f32)));
+                self.draw_pixel(xp as usize, yp as usize, color);
+            }
+        }
+    }
+    pub fn draw_rect_rot<Shade:Shader>(&mut self, x:i32, y:i32, w:i32, h:i32, rot:f64,shader:&Shade){
+        let dx = Vec2r::new(w as f32/2.0, h as f32/2.0);
+        let dy = Vec2r::new(w as f32/2.0, -h  as f32/2.0);
+        let rotr = Mat2::rotmat(rot);
+        println!("{:#?}",rotr);
+        let xp = (rotr*dx).to_int();
+        let yp = (rotr*dy).to_int();
+        let c = Vec2::new(x,y);
+        let p1 = c+xp;
+        let p2 = c-yp;
+        let p3 = c+yp;
+        let p4 = c-xp;
+        println!("{:#?},{:#?}. {:#?}, {:#?}",p1, p2, p3, p4);
+        let u1 = Vec2r::new(0.0, 0.0);
+        let u2 = Vec2r::new(1.0, 0.0);
+        let u3 = Vec2r::new(0.0, 1.0);
+        let u4 = Vec2r::new(1.0, 1.0);
+        self.draw_triangle_shader(p1, p2, p3, u1, u2, u3, shader);
+        self.draw_triangle_shader(p4, p2, p3, u4, u2, u3, shader);
+    }
+    pub fn load(name:&str)->Result<Self, Box<dyn Error>>{
+        let img = load_image::load_path(name)?;
+        let h = img.height;
+        let w = img.width;
+        let (ig ,_)= img.into_rgba();
+        let mut out = Self::new(h,w);
+        for yp in 0..h{
+            for x in 0..w{
+                let y = h-yp-1;
+                let ct = ig[yp][x];
+                let col = Color{r:ct.r, g:ct.g, b:ct.b, a:ct.a};
+                out.draw_pixel(x, y, col);
+            }
+        }
+        Ok(out)
+    }
+}
+
+pub trait Shader{
+    fn kernel(&self,screen_location:Vec2, text_coord:Vec2r)->Color;
 }
--- towngen/src/main.rs
+++ towngen/src/main.rs
@@ -1,4 +1,61 @@
 pub mod draw;
+pub mod math;
+use minifb::{Key, Window, WindowOptions};
+
+use crate::draw::{Color, Image, Shader, Vec2, Vec2r};
+struct Shade{
+    tex:Image
+}
+impl Shade{
+    pub fn new(tex:&str)->Self{
+        let tex = Image::load(tex).unwrap();
+        Self{tex}
+    }
+}
+impl Shader for Shade{
+    fn kernel(&self,screen_location:Vec2, tex_coord:draw::Vec2r)->Color {
+            self.tex.get(tex_coord.x as f64, tex_coord.y as f64)
+    }
+}
+pub fn spinny(){
+        let mut img = Image::new(1000, 1000);
+        let mut window = Window::new(
+            "Test - ESC to exit",
+            img.width as usize,
+            img.height as usize,
+            WindowOptions::default(),
+        )
+        .unwrap_or_else(|e| {
+            panic!("{}", e);
+        });
+        window.set_target_fps(60);
+        let mut rt = 0.0;
+        let sh = Shade::new("nyancat.png");
+        while window.is_open() && !window.is_key_down(Key::Escape) {
+                img.clear(Color {
+                    r: 255,
+                    g: 255,
+                    b: 255, 
+                    a: 255,
+                });
+                rt += 1.0/60.0;
+                img.draw_rect_rot(500,500, 400,400,rt ,&sh);
+            img.draw(&mut window);
+        }
+}
+pub fn old(){
+    let mut img = Image::new(1000, 1000);
+    img.clear(Color {
+        r: 255,
+        g: 255,
+        b: 255,
+        a: 255,
+    });
+    let sh = Shade::new("nyancat.png");
+    img.draw_rect_rot(500,500, 400,400,0.0, &sh);
+    img.export("test.ppm");
+    img.draw_forever();
+}
 fn main() {
-    println!("Hello, world!");
+    spinny();
 }
